21faaf5e73e81dc308bd33d88bbe4037386516d7
ERDesigns Software <ernstreidinga85@gmail.com>
2024-10-20 12:35:51 +0200

    chore: Update ESLint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,7 @@ module.exports = {
     ],
     "rules": {
         "@typescript-eslint/ban-types": "off",
-        "@typescript-eslint/no-explicit-any": "off"
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-this-alias": "off",
     }
 }
