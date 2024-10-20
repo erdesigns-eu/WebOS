77eb56d296ad605f2eb6e2d106728217ad62658b
ERDesigns Software <ernstreidinga85@gmail.com>
2024-10-20 12:29:02 +0200

    chore: Update ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,5 +29,7 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/no-explicit-any": "off"
     }
 }
